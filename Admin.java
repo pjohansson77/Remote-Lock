94d20330ba45dc02ab9db3da05c216f5e68ae0b7
Andreeh <Hoog16@gmail.com>
2014-05-12 09:59:18 +0200

    added admin and admingui

---
+++ Admin.java
@@ -0,0 +1,134 @@
+package lock;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+
+/**
+ * An Admin class that handles all the requests from the AdminGUI
+ * @author Andree Höög, Jesper Hansen, Peter Johansson
+ *
+ */
+public class Admin {
+	private AdminGUI gui;  // används ej än men kan komma att göra om en textfield används i adminGUI istället. 
+	private User[] users;
+	private HashtableOH <String, User> table;
+	private final String SELECT_USER = "You have to select a user!";
+
+	/**
+	 * A constructor that receives a reference to the AdminGUI
+	 * and a reference to the ListenForClient which contains the hashtable
+	 * @param gui a reference to the AdminGUI-class
+	 * @param server a reference to the ListenForClients-class
+	 */
+	public Admin(AdminGUI gui, ListenForClients server) {
+		this.gui = gui;
+		this.table = server.getTable();
+		
+	}
+	
+	/**
+	 * A function that updates and creates a list of users from the hashtable
+	 */
+	public void updateUserList() {
+		int index = 0;
+		User user;
+		users = new User[table.size()];
+	    Iterator<User> values = table.values();
+	    while(values.hasNext()) {
+	    	user = values.next();
+	    	users[index] = user;
+	    	index++;
+	    }
+	}
+	
+	/**
+	 * A function that creates a new DefaultListModel and adds the
+	 * names of all users to it.
+	 * @return listModel a DefaultListModel containing all the names of the users. 
+	 */
+	public DefaultListModel getModel() { 
+		updateUserList();
+		DefaultListModel listModel = new DefaultListModel();
+		for (int i=0; i< users.length; i++) {
+			listModel.addElement(users[i].getName());
+		}
+		return listModel;
+		
+	}
+
+	/**
+	 * A function that removes a user from the database and the hashtable
+	 * @param selectedIndex the index of the user in the list 
+	 */
+	public void remove(int selectedIndex) {
+		if (selectedIndex >= 0) {
+			getModel().remove(selectedIndex);
+			MySQL.deleteSpecificUser( table, users[selectedIndex].getID() );
+			gui.showTextUser("User: " + users[selectedIndex].getName() + "\nhas been removed.");
+		} else
+			gui.showTextUser(SELECT_USER);
+	}
+	
+	/**
+	 * A function that shows the password for a specific user
+	 * @param selectedIndex the index for a specific user in the array
+	 */
+	public void showPassword(int selectedIndex) {
+		if (selectedIndex >= 0) {
+			gui.showTextUser("User: " + users[selectedIndex].getName() + "\nCurrent Password: " + users[selectedIndex].getPassword());
+		} else
+			gui.showTextUser(SELECT_USER);
+	}
+	
+	/**
+	 * A function that resets the password for a specific user
+	 * @param selectedIndex the index for a specific user in the array
+	 */
+	public void resetPassword(int selectedIndex) {
+		if (selectedIndex >= 0) {
+			MySQL.updateMySQLPassword("0000", users[selectedIndex].getID());
+			table.get( users[selectedIndex].getID() ).setPassword( "0000" );
+			gui.showTextUser("Password has been reset!");
+		} else
+			gui.showTextUser(SELECT_USER);
+	}
+	
+	/**
+	 * A function that changes the temporary password to a random 
+	 * generated password, then stores it in the database
+	 */
+	public void changeTempPassword() {
+		String newTempPassword = "";
+		Random rand = new Random();
+		
+		for(int i = 0; i < 8; i++) {
+			if (rand.nextBoolean()) {
+				newTempPassword += (char)(rand.nextInt(26) + 'A');
+			} else {
+				newTempPassword += (char)(rand.nextInt(26) + 'a');
+			}
+		}
+		
+		MySQL.updateTempPWMySQL(newTempPassword);
+		gui.showTextAdmin("Temporary password changed!");
+		
+			
+	}
+	
+	/**
+	 * A function that shows the current temporary password in the database
+	 */
+	public void showTempPassword() {	
+		String[] tempNamePW = MySQL.readTempMySQL();
+		String username = tempNamePW[0];
+		String password = tempNamePW[1];		
+		gui.showTextAdmin("Username: " + username + "\nPassword: " + password);
+	}
+	
+
+
+	
+}
---
+++ AdminGUI.java
@@ -0,0 +1,244 @@
+package lock;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+/**
+ * Class that handles the graphic user interface for the admin where he/she can remove users, show passwords etc. 
+ * @author Andree Höög, Jesper Hansen, Peter Johansson
+ *
+ */
+public class AdminGUI {
+	private JFrame frame = new JFrame("Administration");
+	private JPanel pnlCont = new JPanel();
+	private JPanel pnlConnect = new JPanel(new GridLayout(2,2));
+	private JPanel pnlMainOptions = new JPanel(new BorderLayout());
+	private JPanel pnlMainOptionButtons = new JPanel(new GridLayout(1,3));
+	private JPanel pnlMainInputSTD = new JPanel();
+	private JPanel pnlUserSettings = new JPanel(new BorderLayout());
+	private JPanel pnlUserSettingsBtns = new JPanel();
+	private JPanel pnlAdminSettings = new JPanel();
+	private JButton btnLogin = new JButton("Login");
+	private JButton btnExit = new JButton("Exit");
+	private JButton btnLogout = new JButton("Logout");
+	private JButton btnUserSettings = new JButton("User Settings");
+	private JButton btnAdminSettings = new JButton("Admin Settings");
+	private JButton btnRemoveUser = new JButton("Remove User");
+	private JButton btnResetPW = new JButton("Reset Password");
+	private JButton btnShowPW = new JButton("Show Current Password");
+	private JButton btnUpdate = new JButton("Update List");
+	private JButton btnChangeTempPW = new JButton("Change Temporary Password");
+	private JButton btnShowTempPW = new JButton("Show Temporary Password");
+	private JTextField txtUsername = new JTextField();
+	private JTextArea txtAreaAdmin, txtAreaUser;
+	private JList list;
+	private JPasswordField txtPassword = new JPasswordField();
+	private CardLayout c1 = new CardLayout();
+	private Admin admin;
+	private ListenForClients server;
+	private JScrollPane scrollPane;
+	
+	/**
+	* Constructs a AdminGUI with a reference to the server
+	* @param server A reference to the server
+	*/
+	public AdminGUI(ListenForClients server) {
+		this.server = server;
+		this.admin = new Admin(this, server);
+		pnlCont.setLayout(c1);
+		pnlMainInputSTD.setLayout(c1);
+		
+		txtUsername.setBorder(BorderFactory.createTitledBorder("Username"));
+		txtPassword.setBorder(BorderFactory.createTitledBorder("Password"));
+		pnlConnect.add(txtUsername);
+		pnlConnect.add(txtPassword);
+		pnlConnect.add(btnLogin);
+		pnlConnect.add(btnExit);
+		
+		pnlMainOptions.add(pnlMainOptionButtons, BorderLayout.NORTH);
+		pnlMainOptions.add(pnlMainInputSTD, BorderLayout.CENTER);
+		
+		// addar knapparna i nordpanelen
+		pnlMainOptionButtons.add(btnLogout);
+		pnlMainOptionButtons.add(btnUserSettings);
+		pnlMainOptionButtons.add(btnAdminSettings);
+		
+		
+		
+		// builds the panel for user settings
+		list = new JList(admin.getModel());
+		list.setBorder(BorderFactory.createTitledBorder("Users"));
+		scrollPane = new JScrollPane(list);	
+		pnlUserSettings.add(scrollPane, BorderLayout.CENTER);		
+		txtAreaUser = new JTextArea();
+		txtAreaUser.setPreferredSize(new Dimension(250,95));
+		txtAreaUser.setBorder(BorderFactory.createTitledBorder("Console"));
+		txtAreaUser.setEditable(false);
+		btnRemoveUser.setPreferredSize(new Dimension(250,35));
+		btnShowPW.setPreferredSize(new Dimension(250,35));
+		btnUpdate.setPreferredSize(new Dimension(250,35));
+		btnResetPW.setPreferredSize(new Dimension(250,35));
+		pnlUserSettingsBtns.setPreferredSize(new Dimension(250,250));
+		pnlUserSettingsBtns.add(btnShowPW);
+		pnlUserSettingsBtns.add(btnResetPW);
+		pnlUserSettingsBtns.add(btnRemoveUser);
+		pnlUserSettingsBtns.add(btnUpdate);
+		pnlUserSettingsBtns.add(txtAreaUser);
+		pnlUserSettingsBtns.setBackground(Color.WHITE);
+		pnlUserSettings.add(pnlUserSettingsBtns, BorderLayout.EAST);
+		
+		
+		// Builds the panel for admin settings
+		txtAreaAdmin = new JTextArea();
+		txtAreaAdmin.setPreferredSize(new Dimension(390,70));
+		txtAreaAdmin.setEditable(false);
+		txtAreaAdmin.setBorder(BorderFactory.createTitledBorder("Console"));
+		pnlAdminSettings.add(txtAreaAdmin);
+		pnlAdminSettings.add(btnShowTempPW);
+		pnlAdminSettings.add(btnChangeTempPW);
+		btnChangeTempPW.setPreferredSize(new Dimension(195,35));
+		btnShowTempPW.setPreferredSize(new Dimension(195,35));
+		pnlAdminSettings.setBackground(Color.WHITE);
+		
+		
+		
+		pnlCont.add(pnlConnect, "1");
+		pnlCont.add(pnlMainOptions, "2");
+		c1.show(pnlCont, "1");
+		
+		
+		// add listener to buttons
+		btnLogin.addActionListener(new ButtonListener());	
+		btnLogout.addActionListener(new ButtonListener());		
+		btnUserSettings.addActionListener(new ButtonListener());	
+		btnAdminSettings.addActionListener(new ButtonListener());
+		btnExit.addActionListener(new ButtonListener());
+		btnRemoveUser.addActionListener(new ButtonListener());
+		btnShowPW.addActionListener(new ButtonListener());
+		btnResetPW.addActionListener(new ButtonListener());
+		btnChangeTempPW.addActionListener(new ButtonListener());
+		btnShowTempPW.addActionListener(new ButtonListener());
+		btnUpdate.addActionListener(new ButtonListener());
+
+		
+		frame.add(pnlCont);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		frame.pack();
+		frame.setSize(400, 100);
+		frame.setResizable(false);
+		frame.getRootPane().setDefaultButton(btnLogin);
+	}
+	
+	/**
+	 * Function that sets the visibility of the frame to 
+	 * true or false
+	 * @param b boolean
+	 */
+	public void showFrame(boolean b) {
+		frame.setVisible(b);
+	}
+	
+	/**
+	 * Function that shows a String in a textfield in adminsettings
+	 * @param str String to be shown
+	 */
+	public void showTextAdmin(String str) {
+		txtAreaAdmin.setText(str);
+	}
+	
+	/**
+	 * Function that shows a String in a textfield in usersettings
+	 * @param str String to be shown
+	 */
+	public void showTextUser(String str) {
+		txtAreaUser.setText(str);
+	}	
+	
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == btnLogin) {
+				if (txtUsername.getText().equals("admin") && charToString(txtPassword.getPassword()).equals("alfa")) {
+					c1.show(pnlCont, "2");
+					pnlMainInputSTD.add(pnlUserSettings, "3");
+					pnlMainInputSTD.add(pnlAdminSettings, "4");
+					frame.setSize(400,50);
+				}
+				else JOptionPane.showMessageDialog(null, "Wrong username or password");
+			}
+			if (e.getSource() == btnLogout) {
+				c1.show(pnlCont, "1");
+				pnlMainInputSTD.remove(pnlUserSettings);
+				pnlMainInputSTD.remove(pnlAdminSettings);
+				clearAllDisplays();
+				frame.setSize(400,100);
+			}
+			if (e.getSource() == btnUserSettings) {
+				c1.show(pnlMainInputSTD, "3");
+				frame.setSize(400,320);
+				
+			}
+			if (e.getSource() == btnAdminSettings) {
+				c1.show(pnlMainInputSTD, "4");
+				frame.setSize(400, 175);
+				list.setModel(admin.getModel());
+			}
+			if (e.getSource() == btnExit) {
+				showFrame(false);
+				server.enableAdminButton(true);
+			}
+			
+			if (e.getSource() == btnRemoveUser) {
+				admin.remove( list.getSelectedIndex() );
+				list.setModel(admin.getModel());
+			}
+			
+			if (e.getSource() == btnShowPW) {
+				admin.showPassword(list.getSelectedIndex());
+			}
+			
+			if (e.getSource() == btnResetPW) {
+				admin.resetPassword(list.getSelectedIndex());
+			}
+			
+			if (e.getSource() == btnChangeTempPW) {
+				admin.changeTempPassword();
+			}
+			
+			if (e.getSource() == btnShowTempPW) {
+				admin.showTempPassword();
+			}
+			
+			if (e.getSource() == btnUpdate) {
+				clearAllDisplays();
+				list.setModel(admin.getModel());
+			}
+			
+		}
+		
+	}	
+	
+	/**
+	 * Function that converts an array of chars to a String
+	 * @param pw an array of chars
+	 * @return password the array as a String instead
+	 */
+	private String charToString(char[] pw) {
+		String password = "";
+		for(int i = 0; i<pw.length; i++) {
+			password += pw[i];
+		}
+		return password;
+	}
+	
+	/**
+	 * Clears the textarea in user settings and admin settings
+	 * + clears the passwordfield
+	 */
+	private void clearAllDisplays() {
+		showTextUser("");
+		showTextAdmin("");
+		txtPassword.setText("");
+	}
+	
+}
